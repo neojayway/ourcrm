47b0f0aeb65bb4fa5f55c54549b33c8e3afdf7f4
King <King_runner@163.com>
2016-10-18 14:46:51 +0800

    删除多余的实体类， 增加本人负责的实体类及Mapper，映射文件， 加入项目所需的原始JSP、jQuery、CSS...文件 login.jsp及生成验证码的image.jsp文件放在webapp路径下， 其余所有原始文件放在WEB-INF下page文件夹中。

---
+++ src/mian/java/org/zhiqiang/lzw/mapping/OperateLogMapper.java
@@ -0,0 +1,19 @@
+package org.zhiqiang.lzw.mapping;
+
+import org.zhiqiang.lzw.entity.OperateLog;
+
+public interface OperateLogMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OperateLog record);
+
+    int insertSelective(OperateLog record);
+
+    OperateLog selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OperateLog record);
+
+    int updateByPrimaryKeyWithBLOBs(OperateLog record);
+
+    int updateByPrimaryKey(OperateLog record);
+}
