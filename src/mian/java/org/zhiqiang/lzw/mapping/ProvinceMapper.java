e7aa33a66434b6610e1a4accbba8161f0a25077f
King <King_runner@163.com>
2016-10-20 11:06:47 +0800

    提交

--- src/mian/java/org/zhiqiang/lzw/mapping/ProvinceMapper.java
+++ src/mian/java/org/zhiqiang/lzw/mapping/ProvinceMapper.java
@@ -2,11 +2,32 @@ package org.zhiqiang.lzw.mapping;
 
 import org.zhiqiang.lzw.entity.Province;
 
+/**
+ * 省份Mapper类
+ * @author Administrator
+ *
+ */
 public interface ProvinceMapper {
+	
+	/**
+	 * 根据主键ID进行删除
+	 * @param id
+	 * @return 影响行数
+	 */
     int deleteByPrimaryKey(Integer id);
 
+    /**
+     * 新增
+     * @param record
+     * @return
+     */
     int insert(Province record);
 
+    /**
+     * 新增
+     * @param record
+     * @return
+     */
     int insertSelective(Province record);
 
     Province selectByPrimaryKey(Integer id);
